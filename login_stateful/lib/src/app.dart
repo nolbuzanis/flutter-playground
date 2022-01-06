5723ade0e4ac07d96b9504d14dda9a51a1af5afe
nolbuzanis <nolanbuzanis@gmail.com>
2022-01-06 11:51:19 -0500

    boilerplate initial app setup

---
+++ login_stateful/lib/src/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      title: 'Log me in!',
+      home: Scaffold(body: Text('Show form here'),
+      )
+    );
+  }
+}
