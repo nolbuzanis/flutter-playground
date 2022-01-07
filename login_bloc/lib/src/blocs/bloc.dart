4627f92300b670640f8c9e474478636298416a02
nolbuzanis <nolanbuzanis@gmail.com>
2022-01-07 14:16:22 -0500

    mixin validators to bloc

--- login_bloc/lib/src/blocs/bloc.dart
+++ login_bloc/lib/src/blocs/bloc.dart
@@ -1,14 +1,21 @@
 import 'dart:async';
+import 'validators.dart';
 
-class Bloc {
+class Bloc with Validators {
   final _emailController = StreamController<String>();
   final _passwordController = StreamController<String>();
+
+// add data from stream
+  Stream<String> get password =>
+      _passwordController.stream.transform(validatePassword);
+  Stream<String> get email => _emailController.stream.transform(validateEmail);
 
 // Change data
   Function(String) get changeEmail => _emailController.sink.add;
   Function(String) get changePassword => _passwordController.sink.add;
 
-// add data from stream
-  Stream<String> get password => _passwordController.stream;
-  Stream<String> get email => _emailController.stream;
+  dispose() {
+    _emailController.close();
+    _passwordController.close();
+  }
 }
